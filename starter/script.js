4d5d57dd9d3995820950cdb0b696907707f27368
mirpur33 <146129859+mirpur33@users.noreply.github.com>
2023-11-23 23:29:50 +0000

    Local storage text saved to the script.js file

--- starter/script.js
+++ starter/script.js
@@ -9,5 +9,7 @@ $(document).ready(function () {
     $(".saveBtn").on("click", function() {
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, text);
     })
 })
