2be8fb2d1262bbe7329e07810a051d746b994d66
mirpur33 <146129859+mirpur33@users.noreply.github.com>
2023-11-23 23:28:09 +0000

    saveBtn on click listener assigned to the script.js file

--- starter/script.js
+++ starter/script.js
@@ -3,4 +3,11 @@ $(document).ready(function () {
     var displayTime = document.querySelector ("#currentDay");
 
     var currentTime = dayjs().format("dddd, MMMM, D, YYYY");
+
+    displayTime.textContent = currentTime;
+
+    $(".saveBtn").on("click", function() {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+    })
 })
