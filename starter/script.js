54de8a81b74c2bd28cab396173aaf6575a5f463e
mirpur33 <146129859+mirpur33@users.noreply.github.com>
2023-11-24 01:28:42 +0000

    Save event and load event added

--- starter/script.js
+++ starter/script.js
@@ -1,46 +1,45 @@
 $(document).ready(function () {
-
-    var displayTime = document.querySelector ("#currentDay");
-
+    var displayTime = document.querySelector("#currentDay");
+    
     var currentTime = dayjs().format("dddd, MMMM D, YYYY h:mm A");
-
+  
     displayTime.textContent = currentTime;
-
-
-
-    function saveEvent() {
-        var blockHour = $(this).closest(".time-block").attr("id");
-        var event = $(this).siblings(".description").val();
-
-        localStorage.setItem(blockHour, event);
-
-        $(".saveBtn").on("click", saveEvent);
-    };
 
     function hourTracker() {
 
         var currentHour = dayjs().hour();
 
-        $(".time-block").each(function() {
-            var blockHour = parseInt($(this).attr("id").split("-")[1]);
+        $('.time-block').each(function() {
+          var blockHour = parseInt($(this).attr('id').split('-')[1]);
 
-            if (blockHour < currentHour) {
-                $(this).addClass("past");
-            } else if (blockHour === currentHour) {
-                $(this).addClass("present");
-            } else {
-                $(this).addClass("future");
-            }
+          if (blockHour < currentHour) {
+            $(this).addClass('past');
+          } else if (blockHour === currentHour) {
+            $(this).addClass('present');
+          } else {
+            $(this).addClass('future');
+          }
         });
-    }
-    hourTracker();
+      }
 
-    function displayText() {
-        $(".time-block").each(function () {
-            var blockHour = $(this).attr("id");
-            $(this).children(".description").val(localStorage.getItem(blockHour));
+      function loadEvents() {
+        $('.time-block').each(function() {
+          var blockHour = $(this).attr('id');
+          var event = localStorage.getItem(blockHour);
+
+          if (event) {
+            $(this).find('.description').val(event);
+          }
         });
-    }
+      }
 
-    displayText();
+      function saveEvent() {
+        var blockHour = $(this).closest('.time-block').attr('id');
+        var event = $(this).siblings('.description').val();
+
+        localStorage.setItem(blockHour, event);
+      }
+
+      
+
 });
